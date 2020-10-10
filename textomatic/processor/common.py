9e64a9afa3cc43de29a64be154b709e4ea81b2b9
Dan Kilman <dan.kilman@forter.com>
2020-10-11 01:23:59 +0300

    fix jq tests

--- textomatic/processor/common.py
+++ textomatic/processor/common.py
@@ -4,9 +4,7 @@ from textomatic.exceptions import ProcessException
 
 
 def run_jq(text, args):
-    final_args = ["jq", "-c"]
-    if args:
-        final_args.append(args)
+    final_args = ["jq", "-c", args or "."]
     text = bytes(text, encoding="utf-8") if text else None
     try:
         return subprocess.check_output(
